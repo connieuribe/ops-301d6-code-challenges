24c8ddd3079d2b004d05bb01e93566688c0f63e8
connieuribe <conniuribe95@gmail.com>
2023-03-17 22:37:22 +0000

    new code challenge 05

---
+++ ops-challenge05.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script: Ops 301 Class 04 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 16 Mar 2023
+# Purpose: Conditionals in Menu Systems
+
+# Main
+
+# Print to the screen the file size of the log files before compression
+# Compress the contents of the log files listed below to a backup directory
+# /var/log/syslog
+# /var/log/wtmp
+# The file name should contain a time stamp with the following format -YYYYMMDDHHMMSS
+# Example: /var/log/backups/syslog-20220928081457.zip
+# Clear the contents of the log file
+# Print to screen the file size of the compressed file
+# Compare the size of the compressed files to the size of the original log files
+
+
+
+#END
