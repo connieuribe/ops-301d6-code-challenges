4f5ab552951fd7afc0e1635ccc51225e38e9c560
connieuribe <conniuribe95@gmail.com>
2023-03-23 02:08:26 +0000

    changes to files

---
+++ ops-challenge08.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 22 Mar 2023
+# Purpose: Working with list
+# -------------------------------
+
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+
+#Main
+
+
+
+
+#End
