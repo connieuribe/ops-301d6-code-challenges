c39cf5ddbf18b0a9726f9f3a17dfe61d526c32b2
connieuribe <conniuribe95@gmail.com>
2023-03-23 22:08:49 +0000

    code challenge 07

--- ops-challenge07.py
+++ ops-challenge07.py
@@ -13,26 +13,21 @@
 # Import libraries
 import os
 
-
-
-# Declaration of variables
-
 ### Read user input here into a variable
+path = input ("Enter a path: ")
 
 # Declaration of functions
-
-### Declare a function here
-
-for (root, dirs, files) in os.walk("testdir"):
-    ### Add a print command here to print ==root==
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print(files)
+def walkFunc(pArg):
+    print("2")
+    for (root, dirs, files) in os.walk(pArg, topdown=True):
+        ### Add a print command here to print ==root==
+        print(root)
+        ### Add a print command here to print ==dirs==
+        print(dirs)
+        ### Add a print command here to print ==files==
+        print(files)
 
 # Main
-
-### Pass the variable into the function here
+walkFunc(path)
 
 # End
