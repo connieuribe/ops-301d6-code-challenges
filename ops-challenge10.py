7fbf76c7301881c81f77459e4477906cf8e1ad89
connieuribe <conniuribe95@gmail.com>
2023-03-25 02:45:11 +0000

    code 10

---
+++ ops-challenge10.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Script: Ops 301 Class 09 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 23 Mar 2023
+# Purpose: Using file handling commands
+# -------------------------------
+
+
+# Using file handling commands, create a Python script that creates a new .txt file, 
+# appends three lines, prints to the screen the first line, then deletes the .txt file.
