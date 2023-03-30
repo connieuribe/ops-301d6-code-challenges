3ebea4acd283fb8f0e75167275899048c0f618ae
connieuribe <conniuribe95@gmail.com>
2023-03-30 02:25:35 +0000

    code 12

---
+++ ops-challenge13.ps1
@@ -0,0 +1,11 @@
+# Script: Ops 301 Class 13 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 29 Mar 2023
+# Purpose: 
+
+# Main
+
+
+# Write a Powershell script that adds the below person to AD.
+# Franz Ferdinand is the TPS Reporting Lead at GlobeX USA in Springfield, OR office. Franz is part of the TPS Department. Franz’s email is ferdi@GlobeXpower.com.
+# Test your script. Verify in ADAC that the user was created with the correct attributes. If anything is missing, delete the user in ADAC and re-attempt from Powershell ISE.
